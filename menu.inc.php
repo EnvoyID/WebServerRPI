b9bd2bf5c60478fa643027b8bbce6146d9818374
Panchenko Alexander <ipanchenko.a@gmail.com>
2019-09-23 22:13:35 +0500

    Переработал стиль, но что то осталось лишнее

--- menu.inc.php
+++ menu.inc.php
@@ -1,9 +1,9 @@
 <!-- Представление всех меню главной страницы -->
 <?php
 	$menu1 = array(
-		"Система префиксов:"=>"index.php?id=prefix", 
-		"Зоны обслуживания отдела маркетинга:"=>"index.php?id=marketing",
-		"Телефонный справочник:"=>"index.php?id=telephon");
+		"Пункт меню1:"=>"index.php?id=item1", 
+		"Пункт меню2:"=>"index.php?id=item2",
+		"Пункт меню3:"=>"index.php?id=item3");
 ?>	
 
 <div>
