f99dfc362a62262e909bffeddfc9587c2ba25c6a
Panchenko Alexander <ipanchenko.a@gmail.com>
2020-02-20 19:50:15 +0500

    del items 3

--- menu.inc.php
+++ menu.inc.php
@@ -2,8 +2,8 @@
 <?php
 	$menu1 = array(
 		"Пункт меню1:"=>"index.php?id=item1", 
-		"Пункт меню2:"=>"index.php?id=item2",
-		"Пункт меню3:"=>"index.php?id=item3");
+		"Пункт меню2:"=>"index.php?id=item2");
+		//"Пункт меню3:"=>"index.php?id=item3");
 ?>	
 
 <div>
